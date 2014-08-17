963df76d33c868e46055c4454e7ee61bd8e03693
riyaan <riyaan.williams@gmail.com>
2014-08-17 14:05:34 +0200

    Restructured the project layout

---
+++ project4/POC/Data/Store/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    public class Product
+    {
+        public string Barcode { get; set; }
+        public string Brand { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public Product() {}
+    }
+}
